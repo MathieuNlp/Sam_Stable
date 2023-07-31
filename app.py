91e2744a94d4419810de2f2fa62319b7be25ac53
Mathieu <mathieu.nalpon@student-cs.fr>
2023-07-31 18:46:06 +0200

    adding tab and new view to project

--- app.py
+++ app.py
@@ -113,8 +113,8 @@ with gr.Blocks() as demo:
 
     def generate_mask(image, selected_points, radio_mask):
         predictor.set_image(image)
-        input_points = np.array(selected_points[:,0], dtype=object)
-        input_labels = np.array(selected_points[:,1], dtype=int)
+        input_points = np.array(selected_points, dtype=object)[0,:]
+        input_labels = np.array(selected_points, dtype=int)[:,1]
         print(selected_points)
         masks, _, _ = predictor.predict(
             point_coords=input_points,
